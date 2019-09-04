b514696ebd1d67b3339adcde512598dee273cf4f
Joscelyn <JoscelynRose@gmail.com>
2019-09-04 16:31:49 -0400

    got server running

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // code away!
-const express = require('express');
 
-const server = express();
-server.use(express.json());
+const server = require('./api/server.js');
 
-const port = 5000;
+const port = 6000;
 
 server.listen(port, () => console.log(`server on port ${port}`));
