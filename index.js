6556453a47dae32e42fcbb37254ff2439a3632cf
Joscelyn <JoscelynRose@gmail.com>
2019-09-04 16:20:44 -0400

    started server. added port

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+const port = 5000;
+
+server.listen(port, () => console.log(`server on port ${port}`));
